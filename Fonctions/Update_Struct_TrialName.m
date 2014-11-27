304f485709318007775545539a87b516a6f9d2af
Xavier Drevelle <xavier.drevelle@gmail.com>
2014-11-27 16:24:52 +0100

    modif

--- Fonctions/Update_Struct_TrialName.m
+++ Fonctions/Update_Struct_TrialName.m
@@ -11,11 +11,11 @@ function [varargout] = Update_Struct_TrialName(varargin)
 for i = 1 : nargin
     eval(['struct_in = varargin{' num2str(i) '};'])
     if isfield(struct_in.Trial(1),'TrialName')
-        list_struct = arrayfun(@(i) struct_in.Trial(i).TrialName,1:length(struct_in.Trial),'uni',0);
+        list_struct = arrayfun(@(i) upper(struct_in.Trial(i).TrialName),1:length(struct_in.Trial),'uni',0);
     else
         champs = fieldnames(struct_in.Trial(1));
         ind = find(arrayfun(@(i) isa(struct_in.Trial(1).(champs{i}),'Signal'),1:length(champs)),1);
-        list_struct = arrayfun(@(i) struct_in.Trial(i).(champs{ind}).TrialName,1:length(struct_in.Trial),'uni',0);
+        list_struct = arrayfun(@(i) upper(struct_in.Trial(i).(champs{ind}).TrialName),1:length(struct_in.Trial),'uni',0);
     end
     eval(['list_struct' num2str(i) ' = list_struct;'])
     
@@ -34,7 +34,7 @@ for i = 1 : nargin
     ind_bad = setdiff(1:length(struct_in.Trial),ind_good);
     struct_out = struct_in;
     struct_out.Trial = struct_in.Trial(ind_good);
-    struct_out.Removed_Trials = struct_in.Trial(ind_bad);
+    struct_out.removedTrials = struct_in.Trial(ind_bad);
     eval(['varargout{' num2str(i) '} = struct_out;'])
 end
 
